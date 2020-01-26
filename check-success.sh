9deafe9a72331dbe50c6324e2de17ad887ad25a9
jabooi <henrik-ahlstrom@hotmail.com>
2020-01-26 22:19:59 +0000

    edited travis and script files

--- check-success.sh
+++ check-success.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl --fail  https://jabooi.github.io/testapp/  # --fail = curl varnar om den den inte når sidan
+curl --fail  $PAGE_URL  # --fail = curl varnar om den den inte når sidan
 if [[ $? -ne 0 ]]; then
         echo "The application has been deployed, but it cannot be reached"
         exit 1
 else
-	echo "Website could be reached, assuming success!"
+	echo "$PAGE_URL could be reached, assuming success!"
 	exit 0
 
 fi
