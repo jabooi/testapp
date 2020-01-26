d664c4a0e2219a8520a80f5d30a778498139eaa3
jabooi <henrik-ahlstrom@hotmail.com>
2020-01-26 21:42:31 +0000

    changed fault in check-success.sh

--- check-success.sh
+++ check-success.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl --fail  https://jabooi.github.io/testapp/  # --fail = curl varnar om den den inte når sidan
-if [[ $? -ne 0 ]];
+if [[ $? -ne 0 ]]; then
         echo "The application has been deployed, but it cannot be reached"
         exit 1
 else
